6fbaec064712cd2cfa6acf786428f3de0bbb9e43
Ergels Gaxhaj <gaxhaje@gmail.com>
2019-03-31 20:05:17 -0500

    working example

--- index.js
+++ index.js
@@ -1,53 +1,80 @@
 // implementation of core jQuery for the server side.
-const cheerio        = require('cheerio');
-// simple HTTP request client with Promise support.
-const requestPromise = require('request-promise');
-// url parser
-const url            = require('url');
-
+const cheerio = require('cheerio');
+const request = require('request');
+const URL = require('url-parse');
 
 // igonre the 1st 2 arguemnts:
 // these are the command path to 'node' and the path to 'index.js'
 const args    = process.argv.slice(2);
 // search criteria
-const website = args[0];
-const keyword = args[1];
-// const depth   = args[2];
+const START_WEBSITE = args[0];
+const KEYWORD = args[1];
+const DEPTH   = args[2];
 
-// cheerio options
-const options = {
-  uri: website,
-  transform: body => cheerio.load(body)
+// let foundOnPages = [];
+let numPagesFound = 0;
+let numPagesVisited = 0;
+let pagesFound = [];
+let pages = [START_WEBSITE];
+let pagesVisited = [];
+
+const url = new URL(START_WEBSITE);
+const baseUrl = url.protocol + '//' + url.hostname;
+
+function crawl(depth) {
+
+  if (typeof depth == 'number') {
+    depth++;
+  } else {
+    depth = 0;
+  }
+
+  // don't visit a page twice
+  let page = pages.pop();
+
+  // if (!page || (depth) > DEPTH) {
+  if (!page) {
+    console.log(`Crawled ${numPagesVisited} pages. Found ${numPagesFound} with the term '${KEYWORD}':`);
+    pagesFound.forEach(p => console.log(p));
+    return;
+  }
+
+  if (pagesVisited.includes(page)) crawl(depth);
+  else visitPage(page, depth, crawl);
 }
 
+function visitPage(uri, depth, cb) {
+  pagesVisited.push(uri);
+  numPagesVisited++;
 
-// use promises so we can search efficiently
-requestPromise(options)
-  .then($ => {
-    // process html as if it were jQuery
+  request(uri, (err, response, body) => {
+    if (response.statusCode !== 200) {
+      cb(depth); // continue crawling
+      return;
+    }
 
-    let links = [];
-    let host = url.parse(website).host;
+    // parse page
+    let $ = cheerio.load(body);
+    let text = $('html > body').text().toLowerCase().replace(/\n/g, ' ');
+    let idx = text.indexOf(KEYWORD.toLowerCase());
 
-    // collect all links
-    $('*').each(function () {
-      let link = $(this).attr('href');
+    if (idx > -1) {
+      numPagesFound++;
+      pagesFound.push(`${uri} => ${KEYWORD}`);
+    }
 
-      console.log(link);
-    });
+    collectLinks($, depth);
 
-    // console.log(links);
-
-    // search for keyword
-    // collect URLs within the same domain up to 'two' levels deep
-    // const words = $('*').map(function (i, elem) {
-    //   return $(this).text();
-    // });
-
-  })
-  .catch(err => {
-    // Crawling error
-    // TODO: use a logging librariy
-    console.log('Crawling error: ', err);
+    cb(depth);
   });
 
+}
+
+function collectLinks($) {
+  let relativeLinks = $("a[href^='/']");
+  relativeLinks.each(function () {
+    pages.push(baseUrl + $(this).attr('href'));
+  });
+}
+
+crawl(0);
